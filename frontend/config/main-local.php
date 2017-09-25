ee86eb06b5d52b6eb19e4f4169641e8ab974248f
faruqfadhil <faruqfadhil7@gmail.com>
2017-09-26 06:13:40 +0700

    disable git ignore

---
+++ frontend/config/main-local.php
@@ -0,0 +1,27 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'fN4m7IMU5I3v3aRV-c4pnoJ1qwmV8H9d',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'allowedIPs' => ['*'],
+    ];
+
+}
+
+return $config;
